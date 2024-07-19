65401a9d7ed4c12de2c82dbbadb0d117838a77e9
zzzytc <120274509+zzzytc@users.noreply.github.com>
2024-07-19 17:01:58 +0800

    remove

--- textgrad/engine/__init__.py
+++ textgrad/engine/__init__.py
@@ -45,7 +45,7 @@ def get_engine(engine_name: str, **kwargs) -> EngineLM:
         return ChatOpenAI(model_string=engine_name, **kwargs)
     elif "claude" in engine_name:
         from .anthropic import ChatAnthropic
-        return ChatAnthropic(model_string=engine_name, is_multimodal=_check_if_multimodal(engine_name), **kwargs)
+        return ChatAnthropic(model_string=engine_name, **kwargs)
     elif "gemini" in engine_name:
         from .gemini import ChatGemini
         return ChatGemini(model_string=engine_name, **kwargs)
